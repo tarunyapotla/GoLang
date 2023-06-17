848da5f4f252c5b6800c6f9c7cb9af36d7c2b17b
tarunyapotla <tarunyapotla@gmail.com>
2023-06-17 18:58:58 +0530

    pointers

---
+++ 06.pointers/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to pointers")
+
+	// var ptr *int
+	// fmt.Println("Value of pointer is, ", ptr)
+
+	myNumber := 23
+	var ptr = &myNumber //(&-reference)
+	fmt.Println("Address of actual pointer is ", ptr)
+	fmt.Println("Value of actual pointer is ", *ptr)
+
+	*ptr = *ptr + 2
+	fmt.Println("New value is: ", myNumber)
+
+}
