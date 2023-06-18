ba6f0ec82ee131ad93a76a8276a09cbb9357cb3d
tarunyapotla <tarunyapotla@gmail.com>
2023-06-18 10:30:17 +0530

    structs

--- 10.structs/main.go
+++ 10.structs/main.go
@@ -8,8 +8,8 @@ func main() {
 	//no inheritance, no super or parent in golang
 	hitesh := User{"Hitesh", "hitesh@go.dev", true, 16}
 	fmt.Println(hitesh)
-	fmt.Printf("hitesh details are: %v\n", hitesh)
 	fmt.Printf("hitesh details are: %+v\n", hitesh)
+	fmt.Printf("hitesh details are: %v\n", hitesh)
 	fmt.Printf("Name is %v and email is %v", hitesh.Name, hitesh.Email)
 }
 
