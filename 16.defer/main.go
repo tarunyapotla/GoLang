e177ba7297604150f9605097a7b66ea652326cca
tarunyapotla <tarunyapotla@gmail.com>
2023-06-19 08:56:05 +0530

    defer

---
+++ 16.defer/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("World") //LIFO
+	defer fmt.Println("one")
+	defer fmt.Println("two")
+
+	fmt.Println("Hello")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Print(i)
+	}
+}
